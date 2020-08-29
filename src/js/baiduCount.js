86c30fe042abac3c2a16f8428323ccf7435b32bf
ddzy <1766083035@qq.com>
2020-08-29 14:10:42 +0800

    :sparkles: add baidu count

---
+++ src/js/baiduCount.js
@@ -0,0 +1,12 @@
+/**
+ * @description 百度统计
+ */
+<script>
+  var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?196b68f787d3e6b90b6399abe7ea3216";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
