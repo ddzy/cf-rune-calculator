9d37c47cd2f9153787372325020700aeaa0dd0e8
ddzy <1766083035@qq.com>
2020-08-29 16:16:33 +0800

    :construction: complete the basic logic

--- src/js/index.js
+++ src/js/index.js
@@ -2,23 +2,31 @@
   const oArmsInput = document.querySelector('#arms');
   const oRateInput = document.querySelector('#rate');
   const oCritInput = document.querySelector('#crit');
+  const oMainInput = document.querySelector('#main');
+  const oBlackInput = document.querySelector('#black');
+  const oDollInput = document.querySelector('#doll');
   const oSubmitBtn = document.querySelector('.calculator__process__submit');
   const oResultTable = document.querySelector('.calculator__result');
   const oResultTableBody = oResultTable.querySelector('tbody');
 
   const data = {
-    arms: '',
-    rate: '',
-    crit: '',
+    arms: '', // 武伤
+    rate: '', // 爆率
+    crit: '', // 爆伤
+    main: '', // 主武器
+    black: '', // 黑骑士
+    doll: '', // 玩偶
     result: 0,
   };
-  // 值列表
   const resultList = [
     {
       arms: 0.6,
       rate: 0.37,
       crit: 5.6,
-      result: 6.144,
+      main: 2,
+      black: 6,
+      doll: 6,
+      result: 26.433,
     },
   ];
   const tools = {
@@ -42,6 +50,71 @@
 
       return rightMultiplier * leftMultiplier;
     },
+    /**
+     * 计算理论 dps
+     * @param {Number} totalArms 总的武器伤害倍数
+     * @param {Number} totalRate 总的爆率倍数
+     * @param {Number} totalCrit 总的爆伤倍数
+     * @param {Number} totalBlack 总的黑骑士伤害倍数
+     */
+    _calc(totalArms, totalRate, totalCrit, totalBlack) {
+      let newTotalCrit = totalCrit - 1;
+      let leftMultiplier = 1 + totalRate * newTotalCrit;
+      let rightMultiplier = totalArms * totalBlack / 1.045;
+
+      return leftMultiplier * rightMultiplier;
+    },
+    /**
+     * 计算黑骑士伤害倍数
+     * @param {Number} blackNum 黑骑士数量
+     */
+    calcBlack(blackNum) {
+      return 1 + blackNum * 0.045;
+    },
+    /**
+     * 计算玩偶伤害倍数
+     * @param {Number} dollNum 玩偶数量
+     */
+    calcDoll(dollNum) {
+      return 1 + dollNum * 0.03;
+    },
+    /**
+     * 计算紫色符文主武器伤害倍数
+     * @param {Number} mainNum 紫色主武器数值
+     */
+    calcMain(mainNum) {
+      return 1 + mainNum;
+    },
+    /**
+     * 计算红色符文的武器伤害倍数
+     * @param {Number} armsNum 红色武器伤害
+     */
+    calcArms(armsNum) {
+      return 1 + armsNum;
+    },
+    /**
+     * 计算总暴击率倍数
+     * @param {Number} rateNum 暴击率数值
+     */
+    calcTotalRate(rateNum) {
+      return 0.03 + rateNum;
+    },
+    /**
+     * 计算总暴击伤害倍数
+     * @param {Number} critNum 暴击伤害数值
+     */
+    calcTotalCrit(critNum) {
+      return 2.5 + critNum;
+    },
+    /**
+     * 计算总武器伤害倍数
+     * @param {Number} arms 红色符文的武器伤害倍数
+     * @param {Number} main 紫色符文的武器伤害倍数
+     * @param {Number} doll 玩偶的伤害倍数
+     */
+    calcTotalArms(arms, main, doll) {
+      return arms * main * doll;
+    },
   };
 
   oArmsInput.addEventListener('input', (e) => {
@@ -52,6 +125,15 @@
   });
   oCritInput.addEventListener('input', (e) => {
     data.crit = Number.parseFloat(e.target.value) / 100;
+  });
+  oMainInput.addEventListener('input', (e) => {
+    data.main = Number.parseInt(e.target.value) / 100;
+  });
+  oBlackInput.addEventListener('input', (e) => {
+    data.black = Number.parseInt(e.target.value);
+  });
+  oDollInput.addEventListener('input', (e) => {
+    data.doll = Number.parseInt(e.target.value);
   });
 
   oSubmitBtn.addEventListener('click', () => {
@@ -64,15 +146,27 @@
       }
     }
 
-    data.result = +(tools.calc(
-      data.arms,
-      data.rate,
-      data.crit
-    )).toFixed(3);
+    // 计算总武器伤害倍数
+    const totalArms = tools.calcTotalArms(
+      tools.calcArms(data.arms),
+      tools.calcMain(data.main),
+      tools.calcDoll(data.doll),
+    );
+    // 计算总爆率倍数
+    const totalRate = tools.calcTotalRate(data.rate);
+    // 计算总爆伤倍数
+    const totalCrit = tools.calcTotalCrit(data.crit);
+    // 计算总黑骑士倍数
+    const totalBlack = tools.calcBlack(data.black);
+
+    data.result = +(tools._calc(totalArms, totalRate, totalCrit, totalBlack)).toFixed(3);
     resultList.push({
       arms: data.arms,
       rate: data.rate,
       crit: data.crit,
+      main: data.main,
+      black: data.black,
+      doll: data.doll,
       result: data.result,
     });
 
@@ -86,6 +180,9 @@
         <td>${v.arms}</td>
         <td>${v.rate}</td>
         <td>${v.crit}</td>
+        <td>${v.main}</td>
+        <td>${v.black}</td>
+        <td>${v.doll}</td>
         <td>${v.result}</td>
       </tr>
     `;
@@ -98,6 +195,9 @@
       arms: data.arms * 100,
       rate: data.rate * 100,
       crit: data.crit * 100,
+      main: data.main * 100,
+      black: data.black,
+      doll: data.doll,
       result: data.result,
       index: resultList.length - 1,
     });
